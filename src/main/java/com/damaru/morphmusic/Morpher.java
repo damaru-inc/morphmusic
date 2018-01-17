e635392050202aacc31a880d72b00c3b0c8ac17d
Michael Davis <michael@damaru.com>
2018-01-17 07:25:15 -0500

    Improvements

--- src/main/java/com/damaru/morphmusic/Morpher.java
+++ src/main/java/com/damaru/morphmusic/Morpher.java
@@ -17,189 +17,204 @@ import com.damaru.morphmusic.model.Section;
 @Component
 public class Morpher {
 
-    private Log log = LogFactory.getLog(Morpher.class);
-    private HashMap<String, Pattern> patternMap = new HashMap<>();
+	private Log log = LogFactory.getLog(Morpher.class);
+	private static final String ID_PATTERN = "%s-%s-%03d";
+	private HashMap<String, Pattern> patternMap = new HashMap<>();
 
-    public void process(Part part) throws MorpherException {
-        log.info("Processing part " + part.getName());
+	public void process(Part part) throws MorpherException {
+		log.info("Processing part " + part.getName());
 
-        validate(part);
+		validate(part);
 
-        List<Note> notes = new ArrayList<>();
-        int currentPosition = 0;
+		for (Pattern pattern : part.getPatterns()) {
+			int patternDuration = pattern.getDuration();
+			for (Note note : pattern.getNotes()) {
+				double proportionalStart = note.getStart() / (double) patternDuration;
+				note.setProportionalStart(proportionalStart);
+				double proportionalDuration = note.getDuration() / (double) patternDuration;
+				note.setProportionalDuration(proportionalDuration);
+			}
+		}
 
-        for (Section section : part.getSections()) {
-            log.info("Processing section " + section.getName());
-            if (section.getEndPattern() == null) {
-                currentPosition = repeatPattern(currentPosition, notes, section);
-            } else {
-                currentPosition = morphPatterns(currentPosition, notes, section);
-            }
-        }
+		List<Note> notes = new ArrayList<>();
+		int currentPosition = 0;
 
-        log.info("Finished processing part " + part.getName());
-        notes.forEach(log::info);
-        log.info("Finished dumping notes.");
-    }
+		for (Section section : part.getSections()) {
+			log.info(String.format("Processing section %s currentPosition: %d ", section.getName(), currentPosition));
+			if (section.getEndPattern() == null) {
+				currentPosition = repeatPattern(currentPosition, notes, section);
+			} else {
+				currentPosition = morphPatterns(currentPosition, notes, section);
+			}
+		}
 
-    public int morphPatterns(int currentPosition, List<Note> notes, Section section) {
-        int steps = section.getSteps();
-        if (steps == 0) {
-            return currentPosition;
-        }
+		log.info("Finished processing part " + part.getName());
+		notes.forEach(log::info);
+		log.info("Finished dumping notes.");
+	}
 
-        // if we have 3 steps, we want percentDone to be: .25, .5 and .75
-        double percentPerStep = 1.0 / (steps + 1);
-        double percentDone = percentPerStep;
-        Pattern startPattern = patternMap.get(section.getStartPattern());
-        Pattern endPattern = patternMap.get(section.getEndPattern());
-        List<Note> startNotes = startPattern.getNotes();
-        List<Note> endNotes = endPattern.getNotes();
-        int numStartNotes = startNotes.size();
-        int numEndNotes = endNotes.size();
-        int durationDiff = endPattern.getDuration() - startPattern.getDuration();
-        int durationFactor = endPattern.getDuration() / startPattern.getDuration();
+	public int morphPatterns(int currentPosition, List<Note> notes, Section section) {
+		int steps = section.getSteps();
+		if (steps == 0) {
+			return currentPosition;
+		}
 
-        for (int i = 0; i < steps; i++) {
-            int durationIncrement = 0;
-            if (durationDiff != 0) {
-                durationIncrement = (int) Math.round(durationDiff * percentDone);
-            } else {
-                durationIncrement = startPattern.getDuration();
-            }
-            
-            int numNotesDroppedFromStart = (int) Math.round(numStartNotes * percentDone);
-            int numNotesAddedFromEnd = (int) Math.round(numEndNotes * percentDone);
-            List<Note> notesThisStep = new ArrayList<>();
-            
-            log.info(String.format("step: %d pos: %d pc: %f di: %d startNotes: %d endNotes: %d", i, currentPosition, percentDone, durationIncrement, numNotesDroppedFromStart, numNotesAddedFromEnd));
+		// if we have 3 steps, we want percentDone to be: .25, .5 and .75
+		double percentPerStep = 1.0 / (steps + 1);
+		double percentDone = percentPerStep;
+		Pattern startPattern = patternMap.get(section.getStartPattern());
+		Pattern endPattern = patternMap.get(section.getEndPattern());
+		List<Note> startNotes = startPattern.getNotes();
+		List<Note> endNotes = endPattern.getNotes();
+		int numStartNotes = startNotes.size();
+		int numEndNotes = endNotes.size();
+		int durationDiff = endPattern.getDuration() - startPattern.getDuration();
+		double durationFactor = endPattern.getDuration() / startPattern.getDuration();
+		log.info(String.format("durationDiff: %d durationFactor: %4f currentPosition: %d", durationDiff, durationFactor, currentPosition));
 
-            int n = 0;
-            for (Note note : startNotes) {
-                if (note.getOrderOut() > numNotesDroppedFromStart) {
-                    Note newNote = new Note(note);
-                    
-                    // if the patterns are of the same length, we don't need to compute scaled durations.
-                    
-                    int start = 0;
-                    if (durationDiff == 0) {
-                        newNote.setStart(currentPosition + note.getStart());
-                    } else {
-                    start = currentPosition
-                            + Math.max(1, (int) Math.round(note.getStart() + durationFactor * percentDone));
-                    int duration = Math.max(1, (int) Math.round(note.getDuration() + durationFactor * percentDone));
-                    newNote.setStart(start);
-                    newNote.setDuration(duration);
-                    }
-                    newNote.setId(String.format("%s-%s-%d", section.getName(), startPattern.getName(), i));
-                    notesThisStep.add(newNote);
-                }
-            }
-            for (Note note : endNotes) {
-                if (note.getOrderIn() <= numNotesAddedFromEnd) {
-                    Note newNote = new Note(note);
-                    
-                   // if the patterns are of the same length, we don't need to compute scaled durations.                    
-                    int start = 0;
-                    if (durationDiff == 0) {
-                        newNote.setStart(currentPosition + note.getStart());
-                    } else {
-                        start = currentPosition
-                                + Math.max(1, (int) Math.round(note.getStart() + durationFactor * (1.0 - percentDone)));
-                        int duration = Math.max(1, (int) Math.round(note.getDuration() + durationFactor * (1.0 - percentDone)));
-                        newNote.setStart(start);
-                        newNote.setDuration(duration);                        
-                    }
-                    newNote.setId(String.format("%s-%s-%d", section.getName(), endPattern.getName(), i));
-                    notesThisStep.add(newNote);
-                }
-            }
+		for (int step = 0; step < steps; step++) {
+			int stepDuration = 0;
+			if (durationDiff != 0) {
+				stepDuration = (int) Math.round(startPattern.getDuration() + durationDiff * percentDone);
+			} else {
+				stepDuration = startPattern.getDuration();
+			}
 
-            notesThisStep.sort((n1, n2) -> {
-                return n1.getStart() - n2.getStart();
-            });
-            
-            notes.addAll(notesThisStep);
-            currentPosition += durationIncrement;
-            percentDone += percentPerStep;
-        }
-        return currentPosition;
-    }
+			int numNotesDroppedFromStart = (int) Math.round(numStartNotes * percentDone);
+			int numNotesAddedFromEnd = (int) Math.round(numEndNotes * percentDone);
+			List<Note> notesThisStep = new ArrayList<>();
 
-    public int repeatPattern(int currentPosition, List<Note> notes, Section section) {
-        int steps = section.getSteps();
-        if (steps == 0) {
-            return currentPosition;
-        }
+			log.info(String.format("step: %d pos: %d pc: %f dur: %d startNotes: %d endNotes: %d", step, currentPosition,
+					percentDone, stepDuration, numStartNotes - numNotesDroppedFromStart, numNotesAddedFromEnd));
 
-        Pattern p = patternMap.get(section.getStartPattern());
-        for (int i = 0; i < steps; i++) {
-            for (Note note : p.getNotes()) {
-                Note newNote = new Note(note);
-                newNote.setStart(currentPosition + note.getStart());
-                newNote.setId(String.format("%s-%s-%d", section.getName(), p.getName(), i));
-                notes.add(newNote);
-            }
-            currentPosition += p.getDuration();
-        }
-        return currentPosition;
-    }
+			for (Note note : startNotes) {
+				if (note.getOrderOut() > numNotesDroppedFromStart) {
+					Note newNote = new Note(note);
 
-    private void validate(Part part) throws MorpherException {
-        HashSet<Pattern> unreferencedPatterns = new HashSet<>();
-        boolean valid = true;
+					// if the patterns are of the same length, we don't need to
+					// compute scaled durations.
 
-        // TODO check for the existence of patterns, sections and notes.
+					int start = 0;
+					if (durationDiff == 0) {
+						newNote.setStart(currentPosition + note.getStart());
+					} else {
+						start = currentPosition
+								+ Math.max(1, (int) Math.round(note.getProportionalStart() * stepDuration));
+						int dur = Math.max(1, (int) Math.round(note.getProportionalDuration() * stepDuration));
+						newNote.setStart(start);
+						newNote.setDuration(dur);
+					}
+					newNote.setId(String.format(ID_PATTERN, section.getName(), startPattern.getName(), step));
+					notesThisStep.add(newNote);
+				}
+			}
+			for (Note note : endNotes) {
+				if (note.getOrderIn() <= numNotesAddedFromEnd) {
+					Note newNote = new Note(note);
 
-        for (Pattern p : part.getPatterns()) {
-            String name = p.getName();
-            Pattern existing = patternMap.get(name);
-            if (existing != null) {
-                valid = false;
-                log.error("Found a duplicate pattern: " + name);
-            } else {
-                patternMap.put(name, p);
-            }
-            unreferencedPatterns.add(p);
-        }
+					// if the patterns are of the same length, we don't need to
+					// compute scaled durations.
+					int start = 0;
+					if (durationDiff == 0) {
+						newNote.setStart(currentPosition + note.getStart());
+					} else {
+						start = currentPosition
+								+ Math.max(1, (int) Math.round(note.getProportionalStart() * stepDuration));
+						int dur = Math.max(1, (int) Math.round(note.getProportionalDuration() * stepDuration));
+						newNote.setStart(start);
+						newNote.setDuration(dur);
+					}
+					newNote.setId(String.format(ID_PATTERN, section.getName(), endPattern.getName(), step));
+					notesThisStep.add(newNote);
+				}
+			}
 
-        for (Section s : part.getSections()) {
-            if (s.getSteps() < 0) {
-                valid = false;
-                log.error(String.format("Section %s has a negative number of steps: %d", s.getName(), s.getSteps()));
-            }
-            String sectionName = s.getName();
-            String patternName = s.getStartPattern();
+			notesThisStep.sort((n1, n2) -> {
+				return n1.getStart() - n2.getStart();
+			});
 
-            if (!patternMap.containsKey(patternName)) {
-                valid = false;
-                log.error(String.format("Section %s references a non-existent pattern %s", sectionName, patternName));
-            } else {
-                unreferencedPatterns.remove(patternMap.get(patternName));
-            }
+			notes.addAll(notesThisStep);
+			currentPosition += stepDuration;
+			percentDone += percentPerStep;
+		}
+		return currentPosition;
+	}
 
-            patternName = s.getEndPattern();
+	public int repeatPattern(int currentPosition, List<Note> notes, Section section) {
+		int steps = section.getSteps();
+		if (steps == 0) {
+			return currentPosition;
+		}
 
-            if (patternName != null) {
-                if (!patternMap.containsKey(patternName)) {
-                    valid = false;
-                    log.error(
-                            String.format("Section %s references a non-existent pattern %s", sectionName, patternName));
-                } else {
-                    unreferencedPatterns.remove(patternMap.get(patternName));
-                }
-            }
-        }
+		Pattern p = patternMap.get(section.getStartPattern());
+		log.info(String.format("repeatPattern %s currentPosition %d duration %d steps %d", section.getStartPattern(), currentPosition, p.getDuration(), steps));
+		for (int i = 0; i < steps; i++) {
+			for (Note note : p.getNotes()) {
+				Note newNote = new Note(note);
+				newNote.setStart(currentPosition + note.getStart());
+				newNote.setId(String.format(ID_PATTERN, section.getName(), p.getName(), i));
+				notes.add(newNote);
+			}
+			currentPosition += p.getDuration();
+		}
+		return currentPosition;
+	}
 
-        for (Pattern pattern : unreferencedPatterns) {
-            log.warn(String.format("Pattern %s is never used.", pattern.getName()));
-        }
+	private void validate(Part part) throws MorpherException {
+		HashSet<Pattern> unreferencedPatterns = new HashSet<>();
+		boolean valid = true;
 
-        if (!valid) {
-            throw new MorpherException("The input file contains errors.");
-        } else {
-            log.info("The input file is valid.");
-        }
+		// TODO check for the existence of patterns, sections and notes.
 
-    }
+		for (Pattern p : part.getPatterns()) {
+			String name = p.getName();
+			Pattern existing = patternMap.get(name);
+			if (existing != null) {
+				valid = false;
+				log.error("Found a duplicate pattern: " + name);
+			} else {
+				patternMap.put(name, p);
+			}
+			unreferencedPatterns.add(p);
+		}
+
+		for (Section s : part.getSections()) {
+			if (s.getSteps() < 0) {
+				valid = false;
+				log.error(String.format("Section %s has a negative number of steps: %d", s.getName(), s.getSteps()));
+			}
+			String sectionName = s.getName();
+			String patternName = s.getStartPattern();
+
+			if (!patternMap.containsKey(patternName)) {
+				valid = false;
+				log.error(String.format("Section %s references a non-existent pattern %s", sectionName, patternName));
+			} else {
+				unreferencedPatterns.remove(patternMap.get(patternName));
+			}
+
+			patternName = s.getEndPattern();
+
+			if (patternName != null) {
+				if (!patternMap.containsKey(patternName)) {
+					valid = false;
+					log.error(
+							String.format("Section %s references a non-existent pattern %s", sectionName, patternName));
+				} else {
+					unreferencedPatterns.remove(patternMap.get(patternName));
+				}
+			}
+		}
+
+		for (Pattern pattern : unreferencedPatterns) {
+			log.warn(String.format("Pattern %s is never used.", pattern.getName()));
+		}
+
+		if (!valid) {
+			throw new MorpherException("The input file contains errors.");
+		} else {
+			log.info("The input file is valid.");
+		}
+
+	}
 }
--- src/main/java/com/damaru/morphmusic/model/Note.java
+++ src/main/java/com/damaru/morphmusic/model/Note.java
@@ -1,86 +1,117 @@
 package com.damaru.morphmusic.model;
 
 public class Note {
-    private String id;
-    private int midiNum;
-    private int start;
-    private int duration;
-    private int dynamic;
-    private int expression;
-    private int orderIn;
-    private int orderOut;
-    
-    public Note() {
-        
-    }
-    
-    public Note(Note note) {
-        midiNum = note.getMidiNum();
-        start = note.getStart();
-        duration = note.getDuration();
-        dynamic = note.getDynamic();
-        expression = note.getExpression();
-        orderIn = note.getOrderIn();
-        orderOut = note.getOrderOut();
-    }
-    
-    
-    public String getId() {
-        return id;
-    }
+	private String id;
+	private int midiNum;
+	private int start;
+	private double proportionalStart;
+	private int duration;
+	private double proportionalDuration;
+	private int dynamic;
+	private int expression;
+	private int orderIn;
+	private int orderOut;
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public Note() {
 
-    public int getMidiNum() {
-        return midiNum;
-    }
-    public void setMidiNum(int midiNum) {
-        this.midiNum = midiNum;
-    }
-    public int getStart() {
-        return start;
-    }
-    public void setStart(int start) {
-        this.start = start;
-    }
-    public int getDuration() {
-        return duration;
-    }
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-    public int getDynamic() {
-        return dynamic;
-    }
-    public void setDynamic(int dynamic) {
-        this.dynamic = dynamic;
-    }
-    public int getExpression() {
-        return expression;
-    }
-    public void setExpression(int expression) {
-        this.expression = expression;
-    }
-    
-    public int getOrderIn() {
-        return orderIn;
-    }
-    public void setOrderIn(int orderIn) {
-        this.orderIn = orderIn;
-    }
-    public int getOrderOut() {
-        return orderOut;
-    }
-    public void setOrderOut(int orderOut) {
-        this.orderOut = orderOut;
-    }
-    @Override
-    public String toString() {
-        return "Note [id=" + id + ", midiNum=" + midiNum + ", start=" + start + ", duration=" + duration + ", orderIn="
-                + orderIn + ", orderOut=" + orderOut + "]";
-    }
-    
-    
+	}
+
+	public Note(Note note) {
+		midiNum = note.getMidiNum();
+		start = note.getStart();
+		// proportionalStart =
+		duration = note.getDuration();
+		proportionalDuration = note.getProportionalDuration();
+		dynamic = note.getDynamic();
+		expression = note.getExpression();
+		orderIn = note.getOrderIn();
+		orderOut = note.getOrderOut();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getMidiNum() {
+		return midiNum;
+	}
+
+	public void setMidiNum(int midiNum) {
+		this.midiNum = midiNum;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public double getProportionalStart() {
+		return proportionalStart;
+	}
+
+	public void setProportionalStart(double proportionalStart) {
+		this.proportionalStart = proportionalStart;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public double getProportionalDuration() {
+		return proportionalDuration;
+	}
+
+	public void setProportionalDuration(double proportionateDuration) {
+		this.proportionalDuration = proportionateDuration;
+	}
+
+	public int getDynamic() {
+		return dynamic;
+	}
+
+	public void setDynamic(int dynamic) {
+		this.dynamic = dynamic;
+	}
+
+	public int getExpression() {
+		return expression;
+	}
+
+	public void setExpression(int expression) {
+		this.expression = expression;
+	}
+
+	public int getOrderIn() {
+		return orderIn;
+	}
+
+	public void setOrderIn(int orderIn) {
+		this.orderIn = orderIn;
+	}
+
+	public int getOrderOut() {
+		return orderOut;
+	}
+
+	public void setOrderOut(int orderOut) {
+		this.orderOut = orderOut;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Note [id=%12s m=%2d st=%3d %5f dur=%3d %5f in=%3d out=%3d]", id, midiNum, start,
+				proportionalStart, duration, proportionalDuration, orderIn, orderOut);
+	}
+
 }
